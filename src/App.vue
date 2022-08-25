9b8c860a7d7ad1a63f3b31336bc6fe9968774ab0
ndragun92 <nemanjadragun92@gmail.com>
2022-08-25 19:35:22 +0200

    feat: integrate auth

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>App</div>
+  <div>
+    <RouterView />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,44 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AuthLoginPage from "../pages/auth/login/AuthLoginPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: AuthLoginPage,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-  ],
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/", "/login"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("user");
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
