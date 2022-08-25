6b343d9042fd8a795398b131337b5f23d189bcb2
ndragun92 <nemanjadragun92@gmail.com>
2022-08-25 23:47:54 +0200

    WIP: Mock battle controller

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,9 @@
   <div class="about">
     <h1>This is an about page</h1>
     <button type="button" @click="onGetCharacter()">Get character</button>
+    <pre>
+      {{ results }}
+    </pre>
     {{ loading }}
     <div v-if="loading">Loading...</div>
     <pre>
@@ -13,10 +16,12 @@
 <script setup lang="ts">
 import { CharactersController } from "@/api/Controllers/Http/Character/CharactersController";
 import { reactive } from "vue";
+import { BattlesController } from "@/api/Controllers/Http/Battle/BattlesController";
 const character = reactive({
   data: {},
 });
 const { index, loading } = CharactersController();
+const { results } = BattlesController();
 
 async function onGetCharacter() {
   const response = await index();
