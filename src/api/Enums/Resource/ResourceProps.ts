2a7704134af36b9bfec64648644353bb37d25c61
ndragun92 <nemanjadragun92@gmail.com>
2022-09-02 12:17:15 +0200

    feat: add arena coins and energy

--- src/api/Enums/Resource/ResourceProps.ts
+++ src/api/Enums/Resource/ResourceProps.ts
@@ -1,6 +1,8 @@
 export enum EnumResourceProps {
   gold = "gold",
   diamonds = "diamonds",
+  arena_coins = "arena_coins",
   energy = "energy",
+  arena_energy = "arena_energy",
   character_id = "character_id",
 }
--- src/api/Interfaces/Resource/ResourceInterface.ts
+++ src/api/Interfaces/Resource/ResourceInterface.ts
@@ -4,6 +4,8 @@ export interface ResourceInterface {
   id: number;
   [EnumResourceProps.gold]: number;
   [EnumResourceProps.diamonds]: number;
+  [EnumResourceProps.arena_coins]: number;
   [EnumResourceProps.energy]: number;
+  [EnumResourceProps.arena_energy]: number;
   [EnumResourceProps.character_id]: number;
 }
